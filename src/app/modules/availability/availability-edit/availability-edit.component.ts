49b4ab573aba0063691b674206df79e954c792d9
Tomasz <tomaszjanik17@gmail.com>
2020-12-07 20:36:24 +0100

    Created availability components

---
+++ src/app/modules/availability/availability-edit/availability-edit.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-availability-edit',
+  templateUrl: './availability-edit.component.html',
+  styleUrls: ['./availability-edit.component.scss']
+})
+export class AvailabilityEditComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/modules/availability/availability-view/availability-view.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-availability-view',
+  templateUrl: './availability-view.component.html',
+  styleUrls: ['./availability-view.component.scss']
+})
+export class AvailabilityViewComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
